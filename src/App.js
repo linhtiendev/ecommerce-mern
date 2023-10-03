038caacb8b3d0f42020576d81bbcc704d1603587
linhtiendev <lelinhtien0707@gmail.com>
2023-10-03 15:05:34 +0700

    react-query integration

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import React, { Fragment } from "react";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import React, { Fragment, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { routes } from "./routes";
 import DefaultComponent from "./components/DefaultComponent/DefaultComponent";
+import { routes } from "./routes";
 
 function App() {
+    // useEffect(() => {
+    //     fetchApi();
+    // }, []);
+    const fetchApi = async () => {
+        const res = await axios.get(
+            `${process.env.REACT_APP_API_URL}/product/get-all`
+        );
+        // const res = await axios.get(
+        //     `http://localhost:3001/api/product/get-all`
+        // );
+
+        return res.data;
+    };
+    const query = useQuery({ queryKey: ["product"], queryFn: fetchApi });
+    console.log("query", query);
     return (
         <div>
             <Router>
--- src/index.js
+++ src/index.js
@@ -5,17 +5,19 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { store } from "./redux/store";
 import { Provider } from "react-redux";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+const queryClient = new QueryClient();
 root.render(
-    <React.StrictMode>
+    // <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
         <Provider store={store}>
             <App />
         </Provider>
-    </React.StrictMode>
+        <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+    // </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
