b977dfe4737b0621c5b3a745227d80bbefdca10b
linhtiendev <lelinhtien0707@gmail.com>
2023-10-14 20:05:17 +0700

    custom useMutation to call API

---
+++ src/hooks/useMutationHook.js
@@ -0,0 +1,8 @@
+import { useMutation } from "@tanstack/react-query";
+
+export const useMutationHook = (fnCallback) => {
+    const mutation = useMutation({
+        mutationFn: fnCallback,
+    });
+    return mutation;
+};
