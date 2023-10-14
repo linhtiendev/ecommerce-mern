34eb7f0b9dc2d583e64bc5500f4ee9bc268f743c
linhtiendev <lelinhtien0707@gmail.com>
2023-10-14 20:06:47 +0700

    use axios to fetch API sign-in, sign-up

---
+++ src/services/UserService.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+// function user login
+export const loginUser = async (data) => {
+    const res = await axios.post(
+        `${process.env.REACT_APP_API_URL}/user/sign-in`,
+        data
+    );
+    return res.data;
+};
+
+// function create user
+export const createUser = async (data) => {
+    const res = await axios.post(
+        `${process.env.REACT_APP_API_URL}/user/sign-up`,
+        data
+    );
+    return res.data;
+};
